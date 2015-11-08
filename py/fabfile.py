1a317bed537acc1d82a27a4e238b649ed22144de
Tim <tjgambs@gmail.com>
2015-11-07 23:01:07 -0600

    Changed fabfile

--- py/fabfile.py
+++ py/fabfile.py
@@ -6,13 +6,13 @@ from fabric.api import local, cd
 def commit_push(message):
 	with cd('../'):
 		#Commits to gh-pages
-		local('git checkout organize')
+		local('git checkout gh-pages')
 		local('git add .')
 		local('git commit -a -m "%s"' % message)
 		local('git push')
 
 		#Commits to master
 		local('git checkout master')
-		local('git rebase organize')
+		local('git rebase gh-pages')
 		local('git push origin master')
-		local('git checkout organize')
+		local('git checkout gh-pages')
